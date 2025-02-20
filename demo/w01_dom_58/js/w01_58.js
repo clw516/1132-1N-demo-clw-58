3bbaa1755ee715fdfa9d14baf4f98f81b8e87484
clw516 <s0902226835@gmail.com>
2025-02-20 21:05:41 +0800

    implement add button

--- demo/w01_dom_58/js/w01_58.js
+++ demo/w01_dom_58/js/w01_58.js
@@ -1,19 +1,51 @@
 const userInput = document.querySelector('.input-number');
-const addBtn = document.querySelector('btn-add');
+const addBtn = document.querySelector('.btn-add');
+const divideBtn = document.querySelector('.btn-divide');
 
+const currentResultOutput = document.querySelector('.current-result');
+const currentCalculationOutput = document.querySelector('.current-calculation');
 
-const currentResultOutput = document.querySelector(''
-const defaultResult = 0
-let current
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-console.log('userInput', userInput);
-
-console.log('addBtn', addBtn);
+//console.log('userInput', userInput);
+//console.log('addBtn' , addBtn);
+//console.log('currentResultOutput' , currentResultOutput);
 
 function getUserInput() {
   return parseInt(userInput.value);
 }
 
-function add() {
-  const operand1 = 
+function outputResult(result, text) {
+  currentResultOutput.textContent = result;
+  currentCalculationOutput.textContent = text;
 }
+
+function add() {
+  const operand1 = parseFloat(currentResult);
+  const operand2 = getUserInput();
+  currentResult = operand1 + operand2;
+  //currentResultOutput.textContent = currentResult;
+  const calText = `${operand1} + ${operand2}`;
+  console.log(`${calText} = ${currentResult}`);
+  //currentCalculationOutput.textContent = calText;
+  outputResult(currentResult, calText);
+}
+
+addBtn.addEventListener('click', add);
+
+function divide() {
+  const operand1 = currentResult;
+  const operand2 = getUserInput();
+  if (operand2 == 0) alert('cannot divide by 0');
+  else {
+    currentResult = (operand1 / operand2).toFixed(2);
+    //currentResultOutput.textContent = currentResult;
+    const calText = `${operand1} / ${operand2}`;
+    console.log(`${calText} = ${currentResult}`);
+    //currentCalculationOutput.textContent = calText;
+    outputResult(currentResult, calText);
+  }
+}
+
+divideBtn.addEventListener('click', divide);
