ed5d5ad1a7b253435d1e1ce90501afee95aec5d6
clw516 <s0902226835@gmail.com>
2025-02-20 20:07:19 +0800

    first commit

---
+++ demo/w01_dom_58/js/w01_58.js
@@ -0,0 +1,19 @@
+const userInput = document.querySelector('.input-number');
+const addBtn = document.querySelector('btn-add');
+
+
+const currentResultOutput = document.querySelector(''
+const defaultResult = 0
+let current
+
+console.log('userInput', userInput);
+
+console.log('addBtn', addBtn);
+
+function getUserInput() {
+  return parseInt(userInput.value);
+}
+
+function add() {
+  const operand1 = 
+}
