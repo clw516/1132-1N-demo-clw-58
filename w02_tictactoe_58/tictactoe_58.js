736df13f8dd83c618d6f2ef00499a1c55d2b9c42
clw516 <s0902226835@gmail.com>
2025-02-27 20:33:13 +0800

    W02-P2: show winning message and tie message

---
+++ w02_tictactoe_58/tictactoe_58.js
@@ -0,0 +1,72 @@
+const o = 'o';
+const x = 'x';
+let turn = 0;
+let done = false;
+
+const container = document.querySelector('.container');
+const alert = document.querySelector('.alert');
+const allLi = document.querySelectorAll('.board li');
+const resetBtn = document.querySelector('.reset');
+console.log(alert);
+console.log(allLi);
+
+// function winMessage = (player) => {
+
+// }
+
+const winMessage = (player) => {
+  if (player === 'o') {
+    container.style.backgroundColor = 'rgba(144,238,144,0.5)';
+    alert.style.backgroundColor = 'rgba(144,238,144,0.5)';
+    alert.style.color = 'green';
+    alert.style.display = 'block';
+    alert.textContent = 'player o wins';
+  } else {
+    container.style.backgroundColor = 'rgba(240,118,128,0.726)';
+    alert.style.backgroundColor = 'rgba(240,118,128,0.726)';
+    alert.style.color = 'green';
+    alert.style.display = 'block';
+    alert.textContent = 'player x wins';
+  }
+};
+
+const tieMessage = () => {
+  alert.style.backgroundColor = '#888';
+  alert.style.color = '#ddd';
+  alert.style.display = 'block';
+  alert.textContent = 'tie';
+};
+
+tieMessage('');
+
+/*
+      <ul class="board">
+        <li>+</li>
+        <li>+</li>
+        <li>+</li>
+        <li>+</li>
+        <li>+</li>
+        <li>+</li>
+        <li>+</li>
+        <li>+</li>
+        <li>+</li>
+      </ul>
+
+      player o win
+      <ul class="board">
+        <li class="o">o</li>
+        <li>+</li>
+        <li class="x">x</li>
+        <li class="o">o</li>
+        <li class="x">x</li>
+        <li class="x">x</li>
+        <li class="o">o</li>
+        <li>+</li>
+        <li class="o">o</li>
+      </ul>
+
+player x win
+
+
+
+*/
