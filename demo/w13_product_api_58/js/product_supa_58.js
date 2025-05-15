7630bc68ebe3aaa6453416071d3c304b9f33ece2
clw516 <s0902226835@gmail.com>
2025-05-15 21:07:47 +0800

    Get Products from local json and from API

---
+++ demo/w13_product_api_58/js/product_supa_58.js
@@ -0,0 +1,43 @@
+import { _supabase } from './clientSupabase_58.js';
+
+const productContainer = document.querySelector('.products-container');
+
+let products_58 = [];
+
+const fetchProducts = async () => {
+  try {
+    let { data, error } = await _supabase.from('product_58').select('*');
+    console.log('data', data);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const displayProducts = (products) => {
+  let productsContent = products
+    .map((product) => {
+      const { id, title, price, remote_img } = product;
+      return `
+        <div class="single-product">
+        <img
+          src=${remote_img}
+          class="single-product-img img"
+          alt=${title}
+        />
+        <footer>
+          <h3 class="name">${id}-${title}</h3>
+          <span class="price">$${price}</span>
+        </footer>
+      </div>
+    `;
+    })
+    .join('');
+  productContainer.innerHTML = productsContent;
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+  products_58 = await fetchProducts();
+  //console.log('projects_58', products_58);
+  displayProducts(products_58);
+});
